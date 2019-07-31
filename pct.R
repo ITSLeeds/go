8d2f0b80acc1ea79ba6218d12d12415e94d8d95e
Malcolm Morgan <mem48@users.noreply.github.com>
2019-07-31 14:56:59 +0100

    First commit

---
+++ pct.R
@@ -0,0 +1,9 @@
+source("")
+setup_R(pkgs = c("remotes",
+                "sf",
+                "tidyverse",
+                "cyclestreets",
+                "tmap",
+                "pct",
+                "stats19",
+                "stplanr")); rm(setup_R)
