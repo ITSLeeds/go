b7414247a839b5e9d94c233f1c323d07271e9cad
Malcolm Morgan <mem48@users.noreply.github.com>
2019-10-14 17:05:52 +0100

    my setup function

---
+++ code/malcolm.R
@@ -0,0 +1,11 @@
+source("https://raw.githubusercontent.com/ITSLeeds/go/master/code/setup_function.R")
+setup_R(pkgs = c("remotes",
+                "sf",
+                "tidyverse",
+                "cyclestreets",
+                "tmap",
+                "pct",
+                "stats19",
+                "stplanr"),
+        pkgs_gh = c("opentripplanner")
+        ); rm(setup_R)
