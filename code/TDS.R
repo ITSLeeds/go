2bb6d719d1847272135748a5d3eaaa598d8517b1
mem48 <>
2020-02-05 14:26:29 +0000

    Add TDS setup code

---
+++ code/TDS.R
@@ -0,0 +1,11 @@
+source("https://raw.githubusercontent.com/ITSLeeds/go/master/code/setup_function.R")
+setup_R(pkgs = c("sf",
+                "tidyverse",
+                "cyclestreets",
+                "tmap",
+                "pct",
+                "stats19",
+                "stplanr",
+                "dodgr",
+                "geodist")
+        ); rm(setup_R)
