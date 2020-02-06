5f3a5545c496aae30c302ccf47e9a7003738df3c
Malcolm Morgan <mem48@users.noreply.github.com>
2020-02-06 13:24:47 +0000

    Switch to assets

--- code/with_logging.R
+++ code/with_logging.R
@@ -15,29 +15,20 @@ if(!exists("ram_warn")){
   ram_warn <- 4000
 }
 
-if(!exists("dir_use")){
-  dir_use <- tempdir()
-}
-
-if(!exists("url_use")){
-  url_use <- "https://github.com/ITSLeeds/go/archive/v0.2.zip"
-}
-
-if(!exists("tag")){
-  tag <- "go-0.2"
-}
 # Download and Run
-utils::download.file(url = url_use,
-              destfile = file.path(dir_use,"ITSgo.zip"),
-              quiet = TRUE)
-utils::unzip(file.path(dir_use,"ITSgo.zip"),
-             exdir = file.path(dir_use))
-source(file.path(dir_use,tag,"code","setup_function.R"))
+# utils::download.file(url = url_use,
+#               destfile = file.path(dir_use,"ITSgo.zip"),
+#               quiet = TRUE)
+# utils::unzip(file.path(dir_use,"ITSgo.zip"),
+#              exdir = file.path(dir_use))
+#source(file.path(dir_use,tag,"code","setup_function.R"))
+source("https://github.com/ITSLeeds/go/releases/download/v0.2/setup_function.R")
 setup_R(pkgs = pkgs,
         pkgs_gh = pkgs_gh,
         rversion = rversion,
         ram_warn = ram_warn)
 
-file.remove(file.path(dir_use,"ITSgo.zip"))
-unlink(file.path(dir_use,tag), recursive = TRUE)
-rm(setup_R, url_use, tag, dir_use, pkgs, pkgs_gh, rversion, ram_warn)
+# file.remove(file.path(dir_use,"ITSgo.zip"))
+# unlink(file.path(dir_use,tag), recursive = TRUE)
+#rm(setup_R, url_use, tag, dir_use, pkgs, pkgs_gh, rversion, ram_warn)
+rm(setup_R, pkgs, pkgs_gh, rversion, ram_warn)
