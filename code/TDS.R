80173d8af0162349baff9ab65b6524dd5f0deb4c
Malcolm Morgan <mem48@users.noreply.github.com>
2021-01-26 11:38:15 +0000

    use new url system

--- code/TDS.R
+++ code/TDS.R
@@ -1,16 +1,16 @@
 # Settings - can be changed
-pkgs <- c("sf",
-          "tidyverse",
-          "tmap",
-          "pct",
-          "stats19",
-          "stplanr",
-          "dodgr",
-          "geodist",
-          "opentripplanner",
-          "igraph",
-          "snakecase")
-pkgs_gh <- c("ITSleeds/osmextract",
-             "luukvdmeer/sfnetworks")
-
-source("https://raw.githubusercontent.com/ITSLeeds/go/master/code/with_logging.R")
+source("https://github.com/ITSLeeds/go/releases/download/v0.3/setup_function.R")
+setup_R(pkgs = c("sf",
+                 "tidyverse",
+                 "tmap",
+                 "pct",
+                 "stats19",
+                 "stplanr",
+                 "dodgr",
+                 "geodist",
+                 "opentripplanner",
+                 "igraph",
+                 "snakecase"),
+        pkgs_gh = c("ITSleeds/osmextract",
+                    "luukvdmeer/sfnetworks"))
+rm(setup_R)
